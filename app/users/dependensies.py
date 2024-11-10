16123b8e8d447e03eed2458b590777e47e3f8d19
Михаил Богачев <bogachevmikhailirkutsk@gmail.com>
2024-11-10 11:44:32 +0800

    add user dependensies

---
+++ app/users/dependensies.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timezone
+from fastapi import Depends, HTTPException, Request, status
+from jose import jwt, JWTError
+
+from app.config import get_auth_data
+from app.exceptions import TokenExpiredException, TokenNoFoundException, NoJwtException, NoUserIdException
+
+
+def get_token(request: Request):
+    token = request.cookies.get('users_access_token')
+    if not token:
+        raise TokenNoFoundException
+    return token
+
+
+async def get_current_user(token: str = Depends(get_token)):
+    try:
+        auth_data = get_auth_data()
+        payload = jwt.decode(token, auth_data['secret_key'], algorithms=auth_data['algorithm'])
+    except JWTError:
+        raise NoJwtException
+
+    expire: str = payload.get('exp')
+    expire_time = datetime.fromtimestamp(int(expire), tz=timezone.utc)
+    if not expire or expire_time < datetime.now(timezone.utc):
+        raise TokenExpiredException
+
+    user_id: str = payload.get('sub')
+    if not user_id:
+        raise NoUserIdException
+
+    user: str = payload.get('sub')
+    if not user_id:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='User not found')
+    return user
