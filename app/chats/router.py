7862d26a74c16dc57c0452f797fef221fc193a7a
Михаил Богачев <bogachevmikhailirkutsk@gmail.com>
2024-11-14 21:44:29 +0800

    ws

--- app/chats/router.py
+++ app/chats/router.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter, Depends, Request
+import asyncio
+from typing import Dict, List
+from fastapi import APIRouter, Depends, Request, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from app.chats.dao import MessagesDAO
+from app.chats.schemas import MessageCreate, MessageRead
 from app.users.dao import UserDAO
 from app.users.dependensies import get_current_user
 from app.users.models import User
@@ -21,3 +25,40 @@ async def get_chat_page(
         'chat.html',
         {'request': request, 'user': user_data, 'users_all': users_all}
     )
+
+
+@router.get('/messages/{user_id}', response_model=List[MessageRead])
+async def get_messages(user_id: int, current_user: User = Depends(get_current_user)):
+    return await MessagesDAO.get_messages_between_users(user_id_1=user_id, user_id_2=current_user.id) or []
+
+
+active_connections: Dict[int, WebSocket] = {}
+
+
+async def notify_user(user_id: int, message: dict):
+    if user_id in active_connections:
+        websocket = active_connections[user_id]
+        await websocket.send_json(message)
+
+
+@router.webscoket('/ws/{user_id}')
+async def webscoket_endpoint(websocket: WebSocket, user_id: int):
+    await websocket.accept()
+    active_connections[user_id] = websocket
+    try:
+        while True:
+            await asyncio.sleep(1)
+    except WebSocketDisconnect:
+        active_connections.pop(user_id, None)
+
+
+@router.post('/messages', response_model=MessageCreate)
+async def send_message(message: MessageCreate, current_user: User = Depends(get_current_user)):
+    # TODO переделать под вебсокет
+    await MessagesDAO.add(
+        sender_id=current_user.id,
+        content=message.content,
+        recipient_id=message.recipient_id
+    )
+
+    return {'recepient_id': message.recipient_id, 'content': message.content, 'status': 'ok', 'msg': 'Message saved!'}
